cd2f1ac69c4fb0af7856d4ef8446b859eee68d2c
idk <hankhill19580@gmail.com>
2022-06-27 01:28:35 -0400

    include I2P peers

--- win64/build.sh
+++ win64/build.sh
@@ -28,6 +28,6 @@ i2p.plugin.native -name=monerod \
 		-exename=monerod.exe \
 		-updateurl="http://idk.i2p/i2p.plugins.monero/monerod-windows.su3" \
 		-website="http://idk.i2p/i2p.plugins.monero/" \
-		-command="monerod.exe --tx-proxy=127.0.0.1:7952 --anonymous-inbound \"*.i2p,127.0.0.1:18083,100\"" \
+		-command="monerod.exe --tx-proxy=127.0.0.1:7952 --anonymous-inbound \"*.i2p,127.0.0.1:18083,100\"  --add-peer core5hzivg4v5ttxbor4a3haja6dssksqsmiootlptnsrfsgwqqa.b32.i2p --add-peer dsc7fyzzultm7y6pmx2avu6tze3usc7d27nkbzs5qwuujplxcmzq.b32.i2p --add-peer sel36x6fibfzujwvt4hf5gxolz6kd3jpvbjqg6o3ud2xtionyl2q.b32.i2p --add-peer yht4tm2slhyue42zy5p2dn3sft2ffjjrpuy7oc2lpbhifcidml4q.b32.i2p" \
 		-license=MIT \
 		-res=./tmp/
