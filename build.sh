f98a726e7042420b5623e1b770818f6a8c44a946
idk <hankhill19580@gmail.com>
2022-06-27 01:31:45 -0400

    always pull lin64 first

--- build.sh
+++ build.sh
@@ -4,16 +4,16 @@ here=$(dirname $(readlink -f "$0"))
 cd "$here"
 echo "$here"
 
-cd linux32 && ./build.sh
-cd $here
-
 cd linux64 && ./build.sh
 cd $here
 
-cd linuxarm7 && ./build.sh
+cd linux32 && ./build.sh
 cd $here
 
 cd linuxarm8 && ./build.sh
+cd $here
+
+cd linuxarm7 && ./build.sh
 cd $here
 
 cd win64 && ./build.sh
