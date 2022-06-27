b3e944dc66854ae147b2440a151f704da9def862
idk <hankhill19580@gmail.com>
2022-06-27 01:08:43 -0400

    more targets

---
+++ mac64/build.sh
@@ -0,0 +1,31 @@
+#! /usr/bin/env sh
+	
+SIGNER_DIR="$HOME/i2p-go-keys/"
+
+wget -c -O monero-mac64.tar.bz2 https://downloads.getmonero.org/cli/mac64
+mkdir -p monero-cli-mac64
+tar -xjf monero-mac64.tar.bz2 -C monero-cli-mac64 --strip-components=1
+
+cp -v i2ptunnel.config monero-cli-mac64
+cp monero-cli-mac64/monerod .
+VERSION=$(../linux64/monero-cli-linux64/monerod --version | sed 's/.*(\(.*\))/\1/' -)
+rm -vf client.yaml plugin.yaml
+mkdir -p tmp-lin/lib
+cp -rv monero-cli-mac64/* tmp-lin/lib
+i2p.plugin.native -name=monerod \
+		-signer=hankhill19580@gmail.com \
+		-signer-dir=$SIGNER_DIR \
+		-version "$VERSION" \
+		-author=hankhill19580@gmail.com \
+		-autostart=true \
+		-clientname=monerod \
+		-consolename="Monero node over I2P" \
+		-name="monerod-mac64" \
+		-delaystart="1" \
+		-desc="Monero over I2P" \
+		-exename=monerod \
+		-updateurl="http://idk.i2p/railroad/monerod-mac64.su3" \
+		-website="http://idk.i2p/i2p.plugins.monero/" \
+		-command="monerod --tx-proxy=127.0.0.1:7952 --anonymous-inbound \"*.i2p,127.0.0.1:18083,100\"" \
+		-license=MIT \
+		-res=./tmp-lin/
